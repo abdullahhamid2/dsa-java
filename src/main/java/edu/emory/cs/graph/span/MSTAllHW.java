cb653336132dff5fac3e830d1176fa537e27c364
Abdullah Hamid <ahamid4@emory.edu>
2020-11-16 12:29:40 -0500

    Data Structures and Algorithms in Java

---
+++ src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
@@ -0,0 +1,14 @@
+package edu.emory.cs.graph.span;
+
+import edu.emory.cs.graph.Graph;
+
+import java.util.List;
+
+/** @author Jinho D. Choi */
+public class MSTAllHW implements MSTAll {
+    @Override
+    public List<SpanningTree> getMinimumSpanningTrees(Graph graph) {
+        // TODO: to be updated
+        return null;
+    }
+}
