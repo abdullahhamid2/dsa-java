05a264e276cff4e084aa94ab8759d06424b2063f
Abdullah Hamid <ahamid4@emory.edu>
2020-12-02 12:05:15 -0500

    Data Structures and Algorithms in Java

--- src/test/java/edu/emory/cs/graph/span/MSTAllHWTest.java
+++ src/test/java/edu/emory/cs/graph/span/MSTAllHWTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.emory.cs.graph.span;
 
 import edu.emory.cs.graph.Edge;
@@ -23,7 +38,7 @@ public class MSTAllHWTest {
         }
     }
 
-    //	@Test
+    	@Test
     public void test() {
         List<Graph> graphs = new ArrayList<>();
         graphs.add(getGraph1a());
@@ -163,6 +178,20 @@ public class MSTAllHWTest {
 
         return graph;
     }
+    Graph getGraphAbdullah()
+    {
+        Graph graph = new Graph(5);
+
+        graph.setUndirectedEdge(0, 1, 1);
+        graph.setUndirectedEdge(1, 2, 1);
+        graph.setUndirectedEdge(2, 3, 1);
+        graph.setUndirectedEdge(3, 4, 1);
+        graph.setUndirectedEdge(4, 0, 1);
+
+        return graph;
+    }
+
+
 
 
     Graph getCompleteGraph(int V, Random rand) {
